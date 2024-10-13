bdadf529b323695783cb1b6853406baccf1a3073
martin <t4pz0r@gmail.com>
2024-10-13 20:00:47 +0200

    remove blog

--- src/consts.ts
+++ src/consts.ts
@@ -3,7 +3,6 @@ import type { Site, Metadata, Socials } from "@types";
 export const SITE: Site = {
   NAME: "Astro Nano",
   EMAIL: "markhorn.dev@gmail.com",
-  NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
@@ -13,11 +12,6 @@ export const HOME: Metadata = {
   DESCRIPTION: "Astro Nano is a minimal and lightweight blog and portfolio.",
 };
 
-export const BLOG: Metadata = {
-  TITLE: "Blog",
-  DESCRIPTION: "A collection of articles on topics I am passionate about.",
-};
-
 export const WORK: Metadata = {
   TITLE: "Work",
   DESCRIPTION: "Where I have worked and what I have done.",
@@ -25,20 +19,21 @@ export const WORK: Metadata = {
 
 export const PROJECTS: Metadata = {
   TITLE: "Projects",
-  DESCRIPTION: "A collection of my projects, with links to repositories and demos.",
+  DESCRIPTION:
+    "A collection of my projects, with links to repositories and demos.",
 };
 
 export const SOCIALS: Socials = [
-  { 
+  {
     NAME: "twitter-x",
     HREF: "https://twitter.com/markhorn_dev",
   },
-  { 
+  {
     NAME: "github",
-    HREF: "https://github.com/markhorn-dev"
+    HREF: "https://github.com/markhorn-dev",
   },
-  { 
+  {
     NAME: "linkedin",
     HREF: "https://www.linkedin.com/in/markhorn-dev",
-  }
+  },
 ];
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,5 @@
 import { defineCollection, z } from "astro:content";
 
-const blog = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    date: z.coerce.date(),
-    draft: z.boolean().optional()
-  }),
-});
-
 const work = defineCollection({
   type: "content",
   schema: z.object({
@@ -28,8 +18,8 @@ const projects = defineCollection({
     date: z.coerce.date(),
     draft: z.boolean().optional(),
     demoURL: z.string().optional(),
-    repoURL: z.string().optional()
+    repoURL: z.string().optional(),
   }),
 });
 
-export const collections = { blog, work, projects };
+export const collections = { work, projects };
