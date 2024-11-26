4f3e0fcd7c441aa8da902973ba980bf37d84a3a1
martin <t4pz0r@gmail.com>
2024-11-26 21:48:51 +0100

    restart

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
+// @ts-check
+import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
 export default defineConfig({
-  site: "https://msy.dev",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [tailwind()]
 });
